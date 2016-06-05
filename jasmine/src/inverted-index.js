2bf56b1a3b470b699ccf7003adf9260083a02270
andela-jwarugu <joy.warugu@andela.com>
2016-06-05 15:51:00 +0300

    Change create index method

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,15 +1,22 @@
-var fs = require('fs');
+//var fs = require('fs');
 
 function Index(){
 	'use strict'
-	
+	this.fileContents = {};
 	this.createIndex = function(filePath){
-		fs.readFile(filePath , function doneReading(err, fileContents){
-			
-			console.log(fileContents.toString());
-		})
+		// var results = fs.readFile(filePath , 'utf8', function doneReading(err, fileContents){
+		// console.log(fileContents.split(' '));
+		// })
+		this.fileContents = require(filePath);
+		return JSON.stringfy(this.fileContents);
+	}
+
+	this.getIndex = function(){
+		console.log(this.fileContents);
+		
 	}
 }
 
 var indexObj =new Index();
 indexObj.createIndex('../books.json');
+indexObj.getIndex();
