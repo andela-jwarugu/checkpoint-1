d0d77cf75040768cc9617a68d47791febb327c77
andela-jwarugu <joy.warugu@andela.com>
2016-06-06 01:02:37 +0300

    Chucked the require

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,14 +1,23 @@
-//var fs = require('fs');
-
 function Index(){
 	'use strict'
+
 	this.fileContents = {};
-	this.createIndex = function(filePath){
-		// var results = fs.readFile(filePath , 'utf8', function doneReading(err, fileContents){
-		// console.log(fileContents.split(' '));
-		// })
-		this.fileContents = require(filePath);
-		return this.fileContents;
+	//this.index = {};
+
+	this.createIndex = function(){
+		//read file contents and convert JSON to string then change case and split into words
+		this.fileContents = JSON.stringify([
+  {
+    "title": "Alice in Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  }
+]
+).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
 	}
 
 	this.getIndex = function(){
@@ -17,6 +26,7 @@ function Index(){
 	}
 }
 
-var indexObj =new Index();
-indexObj.createIndex('../books.json');
-indexObj.getIndex();
+// var indexObj = new Index();
+// indexObj.createIndex();
+// indexObj.getIndex();
+
