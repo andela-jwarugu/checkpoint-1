ae4903195c31e52ba69e6ad99b05a0a3d2b3b678
andela-jwarugu <joy.warugu@andela.com>
2016-06-06 01:01:52 +0300

    Working createIndex tst

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,5 +1,24 @@
-var sayMyName = function(name) {
-	"use strict";
-    console.log("Your name is " + name); 
-};
-sayMyName("Joy"); 
+describe('indexObj', function(){
+	var indexObj = new Index;
+	describe('createIndex', function(){
+		// indexObj.getIndex();
+		it('fileContents should be empty before calling createIndex()', function(){
+			var keys = Object.keys(indexObj.fileContents);
+			expect(keys.length).toBe(0);
+		})
+		it('reads json file and asserts file is not empty', function(){
+			indexObj.createIndex();
+			//console.log(indexObj.fileContents);
+			var arr = indexObj.fileContents;
+			expect(arr.length).not.toBe(0);
+			expect(arr.indexOf("joy")).toBe(-1);
+		})
+	})
+	// describe('getIndex',function(){
+	// 	it('returns a stirng of file contents',function(){
+	// 		expect(indexObj.getIndex().toBe(typeof(String)));
+	// 	})
+	// })
+})
+
+
