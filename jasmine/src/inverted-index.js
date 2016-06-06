9d817fec2e0136f1ac665387dd9fcc6ba911df90
andela-jwarugu <joy.warugu@andela.com>
2016-06-07 00:35:34 +0300

    Add stop words

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -33,7 +33,27 @@ function Index(){
 	}
 
 	this.formatContent = function(content, index){
-		this.fileContents = content.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
+		//Stop words from http://www.ranks.nl/stopwords
+		var stopWords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren\'t",
+						"as","at","be","because","been","before","being","below","between","both","but","by","can\'t",
+						"cannot","could","couldn\'t","did","didn\'t","do","does","doesn\'t","doing","don\'t","down",
+						"during","each","few","for","from","further","had","hadn\'t","has","hasn\'t","have","haven\'t",
+						"having","he","he\'d","he\'ll","he\'s","her","here","here\'s","hers","herself","him","himself",
+						"his","how","how\'s","i","i\'d","i\'ll","i\'m","i\'ve","if","in","into","is","isn\'t","it",
+						"it\'s","its","itself","let\'s","me","more","most","mustn\'t","my","myself","no","nor","not",
+						"of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own",
+						"same","shan\'t","she","she\'d","she\'ll","she\'s","should","shouldn\'t","so","some","such",
+						"than","that","that\'s","the","their","theirs","them","themselves","then","there","there\'s",
+						"these","they","they\'d","they\'ll","they\'re","they\'ve","this","those","through","to","too",
+						"under","until","up","very","was","wasn\'t","we","we\'d","we\'ll","we\'re","we\'ve","were",
+						"weren\'t","what","what\'s","when","when\'s","where","where\'s","which","while","who","who\'s",
+						"whom","why","why\'s","with","won\'t","would","wouldn\'t","you","you\'d","you'll","you're",
+						"you\'ve","your","yours","yourself","yourselves"];
+
+		var stopString = "\\b" + stopWords.toString().replace(/\,/gi,"\\b|\\b") + "\\b";
+		var re = new RegExp(stopString,"gi");
+
+		this.fileContents = content.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/gi, '').replace(re, '').replace(/\s(?=\s)/gi, "").trim().toLowerCase().split(' ');
 
 		for(var i=0; i<this.fileContents.length ;i++){
 			if(this.invertedIndex[this.fileContents[i]]) {
@@ -45,9 +65,7 @@ function Index(){
 	}
 
 	this.getIndex = function(){
-		//console.log(this.fileContents);
-		console.log(this.invertedIndex);
-		
+		console.log(this.invertedIndex);		
 	}
 }
 
