83ec5a6a38d0622eaf973a8bf546c17b1dab55a6
andela-jwarugu <joy.warugu@andela.com>
2016-06-06 12:18:52 +0300

    Create an index with unique terms

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,8 +1,8 @@
 function Index(){
 	'use strict'
 
-	this.fileContents = {};
-	//this.index = {};
+	this.fileContents = [];
+	this.invertedIndex = {};
 
 	this.createIndex = function(){
 		//read file contents and convert JSON to string then change case and split into words
@@ -17,21 +17,43 @@ function Index(){
 		"text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
 		}
 		];
-		var len = arr.length;
+
 		var str = '';
-		for(var i = 0; i<len; i++){
+		for(var i = 0; i<arr.length; i++){
+			//console.log(i);
 			var obj = arr[i];
 			var keys = Object.keys(obj);
+			//console.log(keys);
+			
 			for(var j = 0; j < keys.length; j++) {
+				//console.log(j + ': ' + obj[keys[j]]);
 				str += obj[keys[j]] + ' ';
 			}
+		this.formatContent(str);
+		//console.log(str);
 		}
-		// this.fileContents = JSON.stringify(arr).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
-		this.fileContents = str.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
+		
+	}
+
+	this.formatContent = function(content){
+		// console.log(this.fileContents);
+		// console.log(this.invertedIndex);
+		this.fileContents = content.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
+		for(var i=0; i<this.fileContents.length ;i++){
+			//console.log(this.invertedIndex);
+			this.invertedIndex[this.fileContents[i]] = "";
+		}
+		// this.fileContents.forEach(function(item){
+		// 	console.log(this.invertedIndex);
+		// 	this.invertedIndex[item] = "";
+		// });
+		// return this.fileContents;
+
 	}
 
 	this.getIndex = function(){
-		console.log(this.fileContents);
+		//console.log(this.fileContents);
+		console.log(this.invertedIndex);
 		
 	}
 }
