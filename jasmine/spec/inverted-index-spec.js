74a2d6fe9c0b49b674fcb5bfc75c0ea29ce2d11f
andela-jwarugu <joy.warugu@andela.com>
2016-06-15 10:12:56 +0300

    Create test the fetch function

--- jasmine/spec/inverted-index-spec.js
+++ jasmine/spec/inverted-index-spec.js
@@ -1,28 +1,30 @@
 describe('indexObj', function(){
-	var indexObj = new Index;
+	var testObj = new Index();
 
   beforeEach(function(done){
-    indexObj.createIndex.done(function(data){
+    testObj.createIndex().then(function(data){
+			testObj.arr = data;
       done();
     });
-  })
+  });
+
 	describe('createIndex', function(){
 
 		it('reads json file and asserts file is not empty', function(){
+			//console.log(arr);
 			expect(arr.length).not.toBe(0);
-			expect(arr.indexOf("joy")).toBe(-1);
-		})
-	})
+			// expect(arr.indexOf("joy")).toBe(-1);
+		});
+	});
 
-	// describe('getIndex',function(){
+ // describe('getIndex',function(){
  //    indexObj.createIndex();
-
-	// 	it('returns an object of file contents',function(){
-	// 		expect(typeof indexObj.getIndex().toBe('string'));
-	// 	})
-
+ //
+ // 	it('returns an object of file contents',function(){
+ // 		expect(typeof indexObj.getIndex().toBe('string'));
+ // 	})
+ //
+ // });
 // indexObj.searchIndex(["alice", "governor", "wonderland", "lord", "rings"]);
 
-
-  })
 })
