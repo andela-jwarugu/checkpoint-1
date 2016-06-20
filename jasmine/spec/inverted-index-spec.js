f8b8641035984454e7a37acc0caf1f2a26c70aa4
andela-jwarugu <joy.warugu@andela.com>
2016-06-21 00:06:37 +0300

    Better search test

--- jasmine/spec/inverted-index-spec.js
+++ jasmine/spec/inverted-index-spec.js
@@ -37,15 +37,29 @@ describe('testObj', function(){
 			it('ensures that index created is correct', function(){
 				expect(testObj.invertedIndex['alice']).toEqual([0]);
 				expect(testObj.invertedIndex['lord']).toEqual([1]);
-				//expect(testObj.invertedIndex['joy']).toBe(-1);
 			});
 		});
 
 		describe('searchIndex', function(){
 
-			it('ensures the correct results when searched', function(){
+			it('ensures that search returns an array', function(){
+				expect(Array.isArray(testObj.searchIndex('alice'))).toBeTruthy();
+			})
+
+			it('ensures the correct results when a single term is searched', function(){
+
+				expect(testObj.searchIndex('alice')).toEqual([0]);
+				expect(testObj.searchIndex('alliance')).toEqual([1]);
+				expect(testObj.searchIndex('joy')).toEqual([-1]);
+			});
+
+			it('ensures the correct results when multiple terms are searched', function(){
+				// array
 				expect(testObj.searchIndex(['alice', 'lord'])).toEqual([0,1]);
+
+				// multiple arguments
 				expect(testObj.searchIndex('alice', 'lord', 'joy')).toEqual([0, 1, -1]);
+
 			});
 		});
 	});
