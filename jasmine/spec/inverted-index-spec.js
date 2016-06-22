9da5df18a20c95604a0a41cd7d8964092506f98f
andela-jwarugu <joy.warugu@andela.com>
2016-06-22 20:09:13 +0300

    Refactor code

--- jasmine/spec/inverted-index-spec.js
+++ jasmine/spec/inverted-index-spec.js
@@ -1,68 +1,72 @@
-describe('testObj', function(){
-	'use strict'
-	var testObj = new Index(), path = '/jasmine/books.json';
+describe('testObj', function() {
+  'use strict';
+  var testObj = new Index(),
+    path = '/jasmine/books.json';
 
-	describe('emptyIndex', function(){
+  describe('emptyIndex', function() {
 
-		it('Ensure inverted index container is empty', function(){
-			expect(testObj.arr).toBeUndefined();
-		});
+    it('Ensure inverted index container is empty', function() {
+      expect(testObj.arr).toBeUndefined();
+    });
 
-	});
+  });
 
 
-	describe('everything else', function(){
+  describe('everything else', function() {
 
-		beforeEach(function(done){
-		  testObj.createIndex(path).then(function(data){
-				testObj.arr = data;
-				testObj.getIndex();
-		    done();
-		  });
-		});
+    beforeEach(function(done) {
+      testObj.createIndex(path).then(function(data) {
+        testObj.arr = data;
+        testObj.getIndex();
+        done();
+      });
+    });
 
-		describe('Read book data', function(){
+    describe('Read book data', function() {
 
-			it('reads json file and asserts file is not empty', function(){
-				expect(testObj.arr).toBeTruthy();
-				expect(testObj.arr.length).toBeGreaterThan(0);
-				expect(typeof testObj).toEqual('object');
-			});
-		});
+      it('reads json file and asserts file is not empty', function() {
+        expect(testObj.arr).toBeTruthy();
+        expect(testObj.arr.length).toBeGreaterThan(0);
+        expect(typeof testObj).toEqual('object');
+      });
+    });
 
-		describe('Populate index', function(){
+    describe('Populate index', function() {
 
-		 	it('ensures that an index object is created',function(){
-		 		expect(typeof testObj.getIndex()).toBe('object');
-		 	});
+      it('ensures that an index object is created', function() {
+        expect(typeof testObj.getIndex()).toBe('object');
+      });
 
-			it('ensures that index created is correct', function(){
-				expect(testObj.invertedIndex['alice']).toEqual([0]);
-				expect(testObj.invertedIndex['lord']).toEqual([1]);
-			});
-		});
+      it('ensures that index created is correct', function() {
+        expect(testObj.invertedIndex.alice).toEqual([0]);
+        expect(testObj.invertedIndex.lord).toEqual([1]);
+      });
+    });
 
-		describe('searchIndex', function(){
+    describe('searchIndex', function() {
 
-			it('ensures that search returns an array', function(){
-				expect(Array.isArray(testObj.searchIndex('alice'))).toBeTruthy();
-			})
+      it('ensures that search returns an array', function() {
+        expect(Array.isArray(testObj.searchIndex('alice'))).toBeTruthy();
+      });
 
-			it('ensures the correct results when a single term is searched', function(){
+      it('ensures the correct results when a single term is searched',
+        function() {
 
-				expect(testObj.searchIndex('alice')).toEqual([0]);
-				expect(testObj.searchIndex('alliance')).toEqual([1]);
-				expect(testObj.searchIndex('joy')).toEqual([-1]);
-			});
+          expect(testObj.searchIndex('alice')).toEqual([0]);
+          expect(testObj.searchIndex('alliance')).toEqual([1]);
+          expect(testObj.searchIndex('joy')).toEqual([-1]);
+        });
 
-			it('ensures the correct results when multiple terms are searched', function(){
-				// array
-				expect(testObj.searchIndex(['alice', 'lord'])).toEqual([0,1]);
+      it('ensures the correct results when multiple terms are searched',
+        function() {
+          // array
+          expect(testObj.searchIndex(['alice', 'lord'])).toEqual([0, 1]);
 
-				// multiple arguments
-				expect(testObj.searchIndex('alice', 'lord', 'joy')).toEqual([0, 1, -1]);
+          // multiple arguments
+          expect(testObj.searchIndex('alice', 'lord', 'joy'))
+            .toEqual([0, 1, -1]);
 
-			});
-		});
-	});
-})
+        });
+    });
+  });
+});
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,90 +1,102 @@
-function Index(){
-  'use strict'
+function Index() {
+  'use strict';
 
   this.invertedIndex = {};
   this.searchResults = [];
 
-  this.createIndex = function(filepath){
+  this.createIndex = function(filepath) {
+    var self = this;
     /*
 
       createIndex method reads the contents of a file whose path is passed as an
       argument in the method. The method returns a promise.
 
     */
-  return fetch(filepath).then(function(response){
+    return fetch(filepath).then(function(response) {
       return response.text();
-      var self = this;
-  	}).then(function(data){
+    }).then(function(data) {
       self.arr = JSON.parse(data);
       return self.arr;
-  	});
+    });
   };
 
-	this.formatContent = function(content){
+  this.formatContent = function(content) {
     /*
 
       This method removes stop words and punctuation marks and returns an array
       of words. Stop words from http://www.ranks.nl/stopwords
 
     */
-		var stopWords = ["a","about","above","after","again","against","all","am",
-            "an","and","any","are","aren\'t", "as","at","be","because","been",
-            "before","being","below","between","both","but","by","can\'t",
-            "cannot","could","couldn\'t","did","didn\'t","do","does","doesn\'t",
-            "doing","don\'t","down","during","each","few","for","from",
-            "further","had","hadn\'t","has","hasn\'t","have","haven\'t",
-            "having","he","he\'d","he\'ll","he\'s","her","here","here\'s",
-            "hers","herself","him","himself","his","how","how\'s","i","i\'d",
-            "i\'ll","i\'m","i\'ve","if","in","into","is","isn\'t","it","it\'s",
-            "its","itself","let\'s","me","more","most","mustn\'t","my","myself",
-            "no","nor","not","of","off","on","once","only","or","other","ought",
-            "our","ours","ourselves","out","over","own","same","shan\'t","she",
-            "she\'d","she\'ll","she\'s","should","shouldn\'t","so","some",
-            "such","than","that","that\'s","the","their","theirs","them",
-            "themselves","then","there","there\'s","these","they","they\'d",
-            "they\'ll","they\'re","they\'ve","this","those","through","to",
-            "too","under","until","up","very","was","wasn\'t","we","we\'d",
-            "we\'ll","we\'re","we\'ve","were","weren\'t","what","what\'s",
-            "when","when\'s","where","where\'s","which","while","who","who\'s",
-            "whom","why","why\'s","with","won\'t","would","wouldn\'t","you",
-            "you\'d","you'll","you're","you\'ve","your","yours","yourself",
-            "yourselves"];
+    var stopWords = ["a", "about", "above", "after", "again", "against",
+      "all", "am", "an", "and", "any", "are", "aren\'t", "as", "at", "be",
+      "because", "been", "before", "being", "below", "between", "both",
+      "but", "by", "can\'t", "cannot", "could", "couldn\'t", "did",
+      "didn\'t", "do", "does", "doesn\'t", "doing", "don\'t", "down",
+      "during", "each", "few", "for", "from", "further", "had", "hadn\'t",
+      "has", "hasn\'t", "have", "haven\'t", "having", "he", "he\'d",
+      "he\'ll", "he\'s", "her", "here", "here\'s", "hers", "herself", "him",
+      "himself", "his", "how", "how\'s", "i", "i\'d", "i\'ll", "i\'m",
+      "i\'ve", "if", "in", "into", "is", "isn\'t", "it", "it\'s",
+      "its", "itself", "let\'s", "me", "more", "most", "mustn\'t", "my",
+      "myself", "no", "nor", "not", "of", "off", "on", "once", "only",
+      "or", "other", "ought", "our", "ours", "ourselves", "out", "over",
+      "own", "same", "shan\'t", "she", "she\'d", "she\'ll", "she\'s",
+      "should", "shouldn\'t", "so", "some",
+      "such", "than", "that", "that\'s", "the", "their", "theirs", "them",
+      "themselves", "then", "there", "there\'s", "these", "they", "they\'d",
+      "they\'ll", "they\'re", "they\'ve", "this", "those", "through", "to",
+      "too", "under", "until", "up", "very", "was", "wasn\'t", "we", "we\'d",
+      "we\'ll", "we\'re", "we\'ve", "were", "weren\'t", "what", "what\'s",
+      "when", "when\'s", "where", "where\'s", "which", "while", "who", "who\'s",
+      "whom", "why", "why\'s", "with", "won\'t", "would", "wouldn\'t", "you",
+      "you\'d", "you'll", "you're", "you\'ve", "your", "yours", "yourself",
+      "yourselves"
+    ];
 
-		var stopString = "\\b" + stopWords.toString()
-            .replace(/\,/gi,"\\b|\\b") + "\\b";
-		var re = new RegExp(stopString,"gi");
-		var doc = content.replace(/W/gi, '').replace(re, '')
-            .replace(/\s(?=\s)/gi, "").trim().toLowerCase().split(' ');
+    var stopString = "\\b" + stopWords.toString()
+      .replace(/\,/gi, "\\b|\\b") + "\\b";
+    var re = new RegExp(stopString, "gi");
+    var doc = content.replace(/W/gi, '').replace(re, '')
+      .replace(/\s(?=\s)/gi, "").trim().toLowerCase().split(' ');
     return doc;
   };
 
-	this.getIndex = function(){
+  this.getIndex = function() {
 
-    for(var index=0; index<this.arr.length; index++){
+    function getKeys(obj) {
+      var str = '';
 
-  		var str = '';
-  		var obj = this.arr[index];
+      Object.keys(obj).map(function(key) {
+        str += ' ' + (obj[key]);
+      });
 
-  		Object.keys(obj).map(function(key){
-  			str += ' ' + (obj[key]);
-  		});
+      return str;
+    }
+
+    var indexObj = this.invertedIndex;
+    for (var index = 0; index < this.arr.length; index++) {
+
+
+      var str = getKeys(this.arr[index]);
 
       var doc = this.formatContent(str);
 
-      for(var i=0; i<doc.length ;i++){
-        if(this.invertedIndex[doc[i]]){
-          if(this.invertedIndex[doc[i]].indexOf(index) === -1) {
-            this.invertedIndex[doc[i]].push(index);
-          }
-        } else {
-          this.invertedIndex[doc[i]] = [index];
+      for (var i = 0; i < doc.length; i++) {
+        var exist = indexObj.hasOwnProperty(doc[i]);
+
+        if (!exist) {
+          indexObj[doc[i]] = [index];
+        }
+
+        if (exist && !indexObj[doc[i]].includes(index)) {
+          indexObj[doc[i]].push(index);
         }
       }
     }
-    	return this.invertedIndex;
-  	};
+    return indexObj;
+  };
 
-	this.searchIndex = function(searchItems){
+  this.searchIndex = function(searchItems) {
     /*
 
       When a search item is passed into the method it returns an array of that
@@ -92,50 +104,59 @@ function Index(){
 
     */
 
-		var results = [];
+    var results = [];
+    var term;
     var self = this;
-    if (Array.isArray(searchItems)){
 
-      for (var i=0; i<searchItems.length; i++){
-  			Object.keys(this.invertedIndex).map(function(key){
+    if (!Array.isArray(searchItems) && typeof searchItems !== 'string') {
+      return "Invalid Input";
+    }
 
-  				if (searchItems[i] === key){
-            if (self.invertedIndex[searchItems[i]].length === 1){
-                results.push(self.invertedIndex[key][0]);
-            }else{
-              results.push(self.invertedIndex[key]);
-            }
-            }
-  				});
-  		}
-  	}
-    else if (typeof searchItems === 'string'){
-      console.log(arguments);
-      var arg = arguments;
-      for(var i=0; i<arg.length; i++){
+    if (Array.isArray(searchItems)) {
 
-        var notfound = true;
-        Object.keys(this.invertedIndex).map(function(key){
-            console.log(arg[i]);
-            if (arg[i] === key){
-              notfound = false;
-              if (self.invertedIndex[arg[i]].length === 1){
-                	results.push(self.invertedIndex[key][0]);
-              }else{
-                results.push(self.invertedIndex[key]);
-              }
-            }
-        });
-        if (notfound) {
+      for (var i = 0; i < searchItems.length; i++) {
+
+        term = searchItems[i];
+        if (!self.invertedIndex.hasOwnProperty(term)) {
           results.push(-1);
+          continue;
         }
+
+        if (self.invertedIndex[term].length === 1) {
+          results.push(self.invertedIndex[term][0]);
+        } else {
+          results.push(self.invertedIndex[term]);
+        }
+
+
       }
+      return results;
     }
-    else{
-      results.push(-1);
+
+
+    if (typeof searchItems === 'string') {
+      var len = arguments.length;
+      for (var i = 0; i < len; i++) {
+
+        term = arguments[i];
+
+        if (!self.invertedIndex.hasOwnProperty(term)) {
+          results.push(-1);
+          continue;
+        }
+
+        if (self.invertedIndex[term].length === 1) {
+          results.push(self.invertedIndex[term][0]);
+        } else {
+          results.push(self.invertedIndex[term]);
+        }
+
+
+      }
+
+      return results;
     }
-    console.log(results);
-    return results;
+
   };
 
 }
